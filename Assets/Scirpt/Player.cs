beffbc6d9055890ebd34c0c159b3fbb12e1169f6
Ousmane8 <ousmane1422@gmail.com>
2018-05-08 20:17:17 +0200

    Terrain
    Construction du terrain en cours —> beta

---
+++ Assets/Scirpt/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private float speed;
+	private float verticalVelocity; 
+	private float gravity;
+
+	private CharacterController controller;
+	private Vector3 moveVector;
+
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveVector = Vector3.zero;
+
+		if (controller.isGrounded) {
+			verticalVelocity = -0.5f;
+		} else
+		{
+			verticalVelocity -= gravity * Time.deltaTime;
+		}
+
+		//	X - Left and Right
+		moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+		//	Y - Up and Down
+		moveVector.y = verticalVelocity;
+		//	Z - Forward and Backward
+		moveVector.z = speed;
+
+		controller.Move (moveVector * Time.deltaTime);
+
+	}
+}
---
+++ Assets/Scirpt/Player1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player1 : MonoBehaviour {
+	private Rigidbody rb;
+	[SerializeField]
+	private float speed;
+
+	private Animator anim;
+
+	[SerializeField]
+	GameObject platform;
+
+	[SerializeField]
+	Text scoreUI;
+
+	int score = 0;
+
+	float _score = 0f;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		rb = this.GetComponent<Rigidbody>();
+		rb.velocity = new Vector3(0f, 0f, speed);
+	}
+		
+	private void ScoreUpdate()
+	{
+		_score += 5f * Time.deltaTime;
+		score = Mathf.RoundToInt(_score);
+		scoreUI.text = score.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			rb.AddForce (0f, 5f, 0f, ForceMode.Impulse);
+			anim.SetBool ("isrunning", false);
+			anim.Play("Jumping");
+		}
+
+		ScoreUpdate ();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Water") 
+		{
+			GameOver ();
+		}
+	}
+
+	private void GameOver()
+	{
+		Debug.Log ("Game is over");
+	}
+}
