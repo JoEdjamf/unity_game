f09eca3dff73d058e338b38f1de6e10b54242e14
jordyedjamf <kibalojordy.edjamfeile@ynov.com>
2018-05-12 21:31:46 +0200

    joueur
    correction de quelques bug

---
+++ Assets/Scripts/Input/Detecteur.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Text;
+namespace Assets.Scripts
+{
+    public interface Detecteur
+    {
+        Direction? DetectDirection();
+    }
+    public enum Direction
+    {
+        Gauche, Droite, Haut, Bas
+    }
+}
